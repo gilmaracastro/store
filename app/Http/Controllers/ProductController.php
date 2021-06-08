a0395d2032917a09090ff08fdbab25cdae6986d4
Gilmara Castro <gilmaracastro2012@gmail.com>
2021-06-08 16:11:53 -0300

    fix: ajusta formatação do arquivo

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,98 +4,124 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use File;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $products = Product::get([
-            'id',
-            'name',
-            'description',
-            'photo',
-        ]);
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		$products = Product::get([
+			'id',
+			'name',
+			'description',
+			'photo',
+		]);
 
-        return view('home', ['products' => $products]);
-    }
+		return view('home', ['products' => $products]);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('forms.product-form');
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		return view('forms.create');
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $product = Product::create($request->all());
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		$product = Product::create($request->all());
 
-        if ($request->hasFile('photo')) {
-            $file = $request->file('photo');
+		if ($request->hasFile('photo')) {
+			$file = $request->file('photo');
 
-            $extension = $file->getClientOriginalExtension();
+			$extension = $file->getClientOriginalExtension();
 
-            $photo = $product->id . '_photo.' . $extension;
-            $request->file('photo')->move(base_path(). '/public/img/', $photo);
-        }
+			$photo = $product->id . '_photo.' . $extension;
+			$request->file('photo')->move(base_path(). '/public/img/', $photo);
+		}
 
-        $product->photo = $photo;
-        $product->save();
+		$product->photo = $photo;
+		$product->save();
 
-        return view('forms.product-form');
-    }
+		return redirect()->action([ProductController::class, 'index']);
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $product = Product::findOrFail($id);
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		$product = Product::findOrFail($id);
 
-        return view('forms.product-show', ['product' => $product]);
-    }
+		return view('forms.show', ['product' => $product]);
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $product = Product::findOrFail($id);
-        return view('forms.product-edit', ['product' => $product]);
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		$product = Product::findOrFail($id);
+		return view('forms.edit', ['product' => $product]);
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		$product = Product::findOrFail($id);
+		$product->update([
+			'name' => $request->name,
+			'description' => $request->description,
+		]);
 
+		if ($request->hasFile('photo')) {
+			$file = $request->file('photo');
+
+			$extension = $file->getClientOriginalExtension();
+
+			$photo = $product->id . '_photo.' . $extension;
+			$request->file('photo')->move(base_path(). '/public/img/', $photo);
+		}
+
+		$product->photo = $photo;
+		$product->save();
+
+		return redirect()->action([ProductController::class, 'index']);
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
 	public function destroy($id)
 	{
 		$product = Product::findOrFail($id);
